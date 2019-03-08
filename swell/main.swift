f1870e4bb376958eeaf5094b724c633c9282d6b8
Will Tyler <will.tyler11@gmail.com>
2019-03-08 23:31:24 +0000

    Implement basic shell!!!

--- swell/main.swift
+++ swell/main.swift
@@ -9,23 +9,44 @@
 import Foundation
 
 
+fileprivate func lookUp(executableName name: String) -> String? {
+	let fileManager = FileManager.default
+	let environment = ProcessInfo.processInfo.environment
+	let path = environment["PATH"]!
+	let paths = path.split(separator: ":").map(String.init)
+
+	for path in paths {
+		let hypotheticalPath = URL(fileURLWithPath: path).appendingPathComponent(name).path
+
+		if fileManager.fileExists(atPath: hypotheticalPath) {
+			return hypotheticalPath
+		}
+	}
+
+	return nil
+}
+
 while true {
 	Shell.prompt()
 
 	if let line = readLine() {
+		let args = line.split(separator: " ").map(String.init)
 		let process = Process()
 		let pipe = Pipe()
 
-		process.executableURL = URL(fileURLWithPath: "/bin/ls")
-		process.standardOutput = pipe.fileHandleForWriting
+		if let executablePath = lookUp(executableName: args[0]) {
+			process.arguments = Array(args[1...])
+			process.executableURL = URL(fileURLWithPath: executablePath)
+			process.standardOutput = pipe.fileHandleForWriting
 
-		try process.run()
-		process.waitUntilExit()
+			try process.run()
+			process.waitUntilExit()
 
-		let data = pipe.fileHandleForReading.availableData
-		let string = String(data: data, encoding: .utf8)!
+			let data = pipe.fileHandleForReading.availableData
+			let string = String(data: data, encoding: .utf8)!.trimmingCharacters(in: .whitespacesAndNewlines)
 
-		print(string)
+			print(string)
+		}
 	}
 
 }
