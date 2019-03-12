e3d8a96867ac3422271ccee07f805d3938b7cb19
Will Tyler <will.tyler11@gmail.com>
2019-03-12 02:07:08 +0000

    Implement source

--- swell/main.swift
+++ swell/main.swift
@@ -61,106 +61,119 @@ sigintSource.setEventHandler(handler: {
 
 sigintSource.activate()
 
+var inputLines = [String]()
+
 while true {
-	prompt()
+	if inputLines.isEmpty {
+		prompt()
 
-	if let line = readLine() {
-		let statement = Statement(line)
-		let commands = statement.commands
-		var input: FileHandle
-		var output: FileHandle
+		if let line = readLine() {
+			inputLines.append(line)
+		}
+	}
 
-		if let inputRedirectPath = statement.inputRedirect {
-			let fileURL = URL(fileURLWithPath: inputRedirectPath)
-			let fileHandle = try FileHandle(forReadingFrom: fileURL)
+	let line = inputLines.removeFirst()
+	let statement = Statement(line)
+	let commands = statement.commands
+	var input: FileHandle
+	var output: FileHandle
 
-			input = fileHandle
+	if let inputRedirectPath = statement.inputRedirect {
+		let fileURL = URL(fileURLWithPath: inputRedirectPath)
+		let fileHandle = try FileHandle(forReadingFrom: fileURL)
+
+		input = fileHandle
+	}
+	else {
+		input = FileHandle.standardInput
+	}
+
+	for (index, command) in commands.enumerated() {
+		let isLastCommand = index == commands.count-1
+		let process = Process()
+
+		process.standardInput = input
+
+		if isLastCommand {
+			if let outputRedirectPath = statement.outputRedirect {
+				let fileURL = URL(fileURLWithPath: outputRedirectPath)
+
+				if !fileManager.fileExists(atPath: fileURL.path) {
+					fileManager.createFile(atPath: fileURL.path, contents: nil, attributes: nil)
+				}
+
+				let fileHandle = try FileHandle(forWritingTo: fileURL)
+
+				process.standardOutput = fileHandle
+			}
+			else {
+				output = FileHandle.standardOutput
+			}
 		}
 		else {
-			input = FileHandle.standardInput
+			let pipe = Pipe()
+
+			output = pipe.fileHandleForWriting
+			input = pipe.fileHandleForReading
+
+			process.standardOutput = output
 		}
 
-		for (index, command) in commands.enumerated() {
-			let isLastCommand = index == commands.count-1
-			let process = Process()
+		process.terminationHandler = { process in
+			runningProcesses.remove(process)
 
-			process.standardInput = input
+			if let input = process.standardInput as? FileHandle, input !== FileHandle.standardInput {
+				input.closeFile()
+			}
+			if let output = process.standardOutput as? FileHandle, output !== FileHandle.standardOutput {
+				output.closeFile()
+			}
+			if let error = process.standardError as? FileHandle, error !== FileHandle.standardError {
+				error.closeFile()
+			}
+		}
 
-			if isLastCommand {
-				if let outputRedirectPath = statement.outputRedirect {
-					let fileURL = URL(fileURLWithPath: outputRedirectPath)
+		switch command.name {
+		case "cd":
+			if command.args.count > 0 {
+				FileManager.default.changeCurrentDirectoryPath(command.args[1])
+			}
+			else {
+				let enviroment = ProcessInfo.processInfo.environment
 
-					if !fileManager.fileExists(atPath: fileURL.path) {
-						fileManager.createFile(atPath: fileURL.path, contents: nil, attributes: nil)
-					}
-
-					let fileHandle = try FileHandle(forWritingTo: fileURL)
-
-					process.standardOutput = fileHandle
+				if let home = enviroment["HOME"] {
+					FileManager.default.changeCurrentDirectoryPath(home)
 				}
-				else {
-					output = FileHandle.standardOutput
+			}
+
+		case "exit":
+			exit(0)
+
+		case "source":
+			if command.args.count == 1 {
+				let path = command.args.first!
+
+				if let content = fileManager.contents(atPath: path), let string = String(data: content, encoding: .utf8) {
+					let sourceLines = string.split(separator: "\n").map(String.init)
+
+					inputLines.append(contentsOf: sourceLines)
+				}
+			}
+
+		default:
+			if let executablePath = lookUp(executableName: command.name) {
+				process.executableURL = URL(fileURLWithPath: executablePath)
+				process.arguments = command.args
+
+				try process.run()
+				runningProcesses.insert(process)
+
+				if isLastCommand {
+					process.waitUntilExit()
 				}
 			}
 			else {
-				let pipe = Pipe()
-
-				output = pipe.fileHandleForWriting
-				input = pipe.fileHandleForReading
-
-				process.standardOutput = output
-			}
-
-			process.terminationHandler = { process in
-				runningProcesses.remove(process)
-
-				if let input = process.standardInput as? FileHandle, input !== FileHandle.standardInput {
-					input.closeFile()
-				}
-				if let output = process.standardOutput as? FileHandle, output !== FileHandle.standardOutput {
-					output.closeFile()
-				}
-				if let error = process.standardError as? FileHandle, error !== FileHandle.standardError {
-					error.closeFile()
-				}
-			}
-
-			switch command.name {
-			case "cd":
-				if command.args.count > 0 {
-					FileManager.default.changeCurrentDirectoryPath(command.args[1])
-				}
-				else {
-					let enviroment = ProcessInfo.processInfo.environment
-
-					if let home = enviroment["HOME"] {
-						FileManager.default.changeCurrentDirectoryPath(home)
-					}
-				}
-
-			case "exit":
-				exit(0)
-
-			case "source":
-				if command.args.count == 1 {
-					let path = command.args.first!
-
-					if let content = fileManager.contents(atPath: path) {
-					}
-				}
-
-			default:
-				if let executablePath = lookUp(executableName: command.name) {
-					process.executableURL = URL(fileURLWithPath: executablePath)
-					process.arguments = command.args
-
-					try process.run()
-					runningProcesses.insert(process)
-
-					if isLastCommand {
-						process.waitUntilExit()
-					}
-				}
+				print("Could not find \(command.name)...")
 			}
 		}
 	}
